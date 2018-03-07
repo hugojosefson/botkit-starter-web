d6fffde898decd94da5612104685048e8de914ed
Hugo Josefson <hugo@josefson.org>
2018-03-07 10:27:26 +0100

    Get some results from openmeal-api.

--- skills/recastai_hears.js
+++ skills/recastai_hears.js
@@ -1,3 +1,12 @@
+import { DateTime } from 'luxon'
+import {
+  getDistributors,
+  getDataProviderSkolmaten,
+  filterDistributors,
+  getMeals,
+  extractOnlyMeals
+} from '@hugojosefson/openmeal-api'
+
 const debug = require('debug')('botkit:recastai_hears')
 
 module.exports = controller => {
@@ -35,7 +44,7 @@ module.exports = controller => {
     'message_received',
     (bot, message) => {
       if (message.entities.location && message.entities.location.length) {
-        message.entities.location.forEach(({ formatted, confidence }) => {
+        message.entities.location.forEach(({ formatted, confidence, raw }) => {
           debug(
             `Heard whats-for-lunch-in-location, and I'm ${100 *
               confidence} % certain they mean ${formatted}.`
@@ -45,6 +54,50 @@ module.exports = controller => {
             `I'm ${100 *
               confidence} % certain you want to know what's for lunch in ${formatted}.`
           )
+
+          const today = DateTime.local().toFormat('yyyy-MM-dd')
+          getDataProviderSkolmaten()
+            .then(skolmaten => {
+              getDistributors(skolmaten)
+                .then(distributors =>
+                  filterDistributors({
+                    distributors,
+                    address: raw
+                  })
+                )
+                .then(distributors =>
+                  distributors.forEach(distributor =>
+                    getMeals({
+                      dataprovider: skolmaten,
+                      distributor,
+                      startDate: today,
+                      endDate: today
+                    })
+                      .then(extractOnlyMeals)
+                      .then(meals => {
+                        meals
+                          .filter(meal => meal.courses && meal.courses.length)
+                          .forEach(meal =>
+                            bot.reply(
+                              message,
+                              `${meal.name} today in ${
+                                distributor.name
+                              } is ${meal.courses
+                                .map(({ name }) => name)
+                                .join('; ')}.`
+                            )
+                          )
+                      })
+                  )
+                )
+            })
+            .catch(e => {
+              debug('There was an error: ' + JSON.stringify(e, null, 2))
+              bot.reply(
+                message,
+                'There was an error: ' + JSON.stringify(e, null, 2)
+              )
+            })
         })
       } else {
         debug('Heard whats-for-lunch-in-location, but no location detected.')
