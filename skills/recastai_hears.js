17c8f4a0ce4529e6a7afc10c74720432036b4d5e
Hugo Josefson <hugo@josefson.org>
2018-03-07 09:46:10 +0100

    Hear whats-for-lunch{,-in-location}

--- skills/recastai_hears.js
+++ skills/recastai_hears.js
@@ -19,4 +19,40 @@ module.exports = controller => {
       )
     }
   )
+
+  debug('Register "hears whats-for-lunch"')
+  controller.hears(['whats-for-lunch'], 'message_received', (bot, message) => {
+    debug('Heard whats-for-lunch')
+    bot.reply(
+      message,
+      "You seem to want to know what's for lunch, but haven't told me where."
+    )
+  })
+
+  debug('Register "hears whats-for-lunch-in-location"')
+  controller.hears(
+    ['whats-for-lunch-in-location'],
+    'message_received',
+    (bot, message) => {
+      if (message.entities.location && message.entities.location.length) {
+        message.entities.location.forEach(({ formatted, confidence }) => {
+          debug(
+            `Heard whats-for-lunch-in-location, and I'm ${100 *
+              confidence} % certain they mean ${formatted}.`
+          )
+          bot.reply(
+            message,
+            `I'm ${100 *
+              confidence} % certain you want to know what's for lunch in ${formatted}.`
+          )
+        })
+      } else {
+        debug('Heard whats-for-lunch-in-location, but no location detected.')
+        bot.reply(
+          message,
+          "You seem to want to know what's for lunch in a specific place, but I could not understand where."
+        )
+      }
+    }
+  )
 }
