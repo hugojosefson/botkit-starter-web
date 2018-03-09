647be0a73172d31b2c4c4b53fccd585b4b0b2aca
Hugo Josefson <hugo@josefson.org>
2018-03-09 15:32:39 +0100

    Start conversation on connection event.

--- src/skills/connection-events.js
+++ src/skills/connection-events.js
@@ -1,12 +1,22 @@
+import { doWhatWeCan, startConvo } from './recastai-hears/whats-for-lunch'
+
 export default controller => {
   controller.on('hello', (bot, message) => {
     // a new session with an unknown user has begun
     bot.reply(message, 'Hi! You are new here.')
+
+    startConvo(bot, message).then(convo => {
+      doWhatWeCan(convo)
+    })
   })
 
   controller.on('welcome_back', (bot, message) => {
     // a known user has started a new, fresh session. Or just reloaded their browser tab.
     bot.reply(message, 'Welcome back!')
+
+    startConvo(bot, message).then(convo => {
+      doWhatWeCan(convo)
+    })
   })
 
   controller.on('reconnect', (bot, message) => {
@@ -15,5 +25,9 @@ export default controller => {
       message,
       'Some sub-space interference just caused our connection to be interrupted. But I am back now.'
     )
+
+    startConvo(bot, message).then(convo => {
+      doWhatWeCan(convo)
+    })
   })
 }
